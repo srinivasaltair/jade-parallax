464fda920196dc9c99d58efb623feb2e6789d295
Srinivas P <srnvs0708@gmail.com>
2015-05-17 14:41:26 +0530

    Initial Commit, Parallax effect with Jade Template

---
+++ _/components/js/script.js
@@ -0,0 +1,43 @@
+$(window).scroll(function(){
+
+	var wScroll = $(this).scrollTop();
+
+	$('.bird-box').css({
+		'transform' : 'translate(0px, -'+ wScroll /30 +'%)'
+	});
+
+	$('.logo').css({
+		'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+	});
+
+	$('.back-bird').css({
+		'transform' : 'translate(0px, '+ wScroll /4+'%)'
+	});
+
+	$('.fore-bird').css({
+		'transform' : 'translate(0px, -'+ wScroll /30+'%)'
+	});
+
+	if(wScroll > $('.clothes-pics').offset().top - ($(window).height()/1.2)){
+
+		$('.clothes-pics figure').each(function (i) {
+			
+			setTimeout(function(){
+				$('.clothes-pics figure').eq(i).addClass('is-showing');
+			}, 150 * (i+1));
+
+		});
+	}
+
+
+	if(wScroll > $('.large-window').offset().top - ($(window).height())){
+
+		$('.large-window').css({'background-position':'center'+ (wScroll - $('.large-window').offset().top) +'px'});
+
+		var opacity = (wScroll - $('.large-window').offset().top + 400) / (wScroll / 5);
+
+		$('.window-tint').css({'opacity': opacity})
+
+	}
+
+});
